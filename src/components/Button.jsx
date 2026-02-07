6dd573c5d083374bf140737b66a50b05cde406c3
sophienocode <sophieprotanki@gmail.com>
2026-02-07 07:26:12 -0800

    (Auth): add Button component with an onClick event handler

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Button() {
+
+    const handleClick = () => console.log(Math.round(Math.random() * 10))
   return (
     <>
-     <button>
+     <button onClick={handleClick} > 
         Click
     </button> 
     </>
